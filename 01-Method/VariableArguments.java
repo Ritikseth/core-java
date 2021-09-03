4abe126c3d28cc43eba812f8bd130a35aca450b9
Ritik Seth <ritik8200@gmail.com>
2021-09-03 16:57:55 +0530

    Added variable arguments basics

--- 01-Method/VariableArguments.java
+++ 01-Method/VariableArguments.java
@@ -2,13 +2,14 @@ import java.util.*;
 import java.lang.*;
 
 public class VariableArguments {
-    static void showList(int start, String ...names){
-        for(int i=0;i<names.length;i++){
-            System.out.println(start+". "+names[i]);
+    static void showList(int start, String... names) {
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(start + ". " + names[i]);
             start++;
         }
     }
+
     public static void main(String[] args) {
-        showList(6,"Ramesh","Suresh","Keshav","Smith","John");
+        showList(6, "Ramesh", "Suresh", "Keshav", "Smith", "John");
     }
 }
