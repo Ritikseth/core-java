bfd0e7c5b844b268b6b62f472fce7ac827747598
Ritik Seth <ritik8200@gmail.com>
2021-09-03 14:58:07 +0530

    Added method overloading

---
+++ 01-Method/method_overloading.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+
+class method_overloading{
+    static int max(int a, int b){
+        return a > b ? a : b;
+    }
+    static int max(int a, int b, int c){
+        return a>b&& a>c? a:(b>c?b:c);
+    }
+    static float max(float a, float b){
+        return a > b?a:b;
+    }
+    static double max(double a, double b){
+        return a > b?a:b;
+    }
+    public static void main(String[] args){
+        System.out.println(max(10,15));
+        System.out.println(max(5f,7f));
+        System.out.println(max(5,20,15));
+        System.out.println(max(5.6,10.8));
+    }
+}
