2bc6f312bfed200c3eae2ed2e10cf61a46bb641a
Ritik Seth <ritik8200@gmail.com>
2021-09-04 18:15:53 +0530

    Types of Properties

---
+++ 02-OOP/Properties.java
@@ -0,0 +1,36 @@
+import java.lang.*;
+import java.util.*;
+
+class Rectangle { // Read & Writable property
+    private double length;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double l) {
+        length = l;
+    }
+}
+
+class Student { // Read Only property
+    private int roll;
+
+    public int getRoll() {
+        return roll;
+    }
+}
+
+class Producer {
+    private int sharedData;
+
+    public void setSharedData(int d) {
+        sharedData = d;
+    }
+}
+
+class Properties {
+    public static void main(String[] args) {
+
+    }
+}
